f3b10348a553ff12091b89827ed3fe66674b4c5b
Pavel Popov <pavelpopov@Pavels-MBP.wifi.gsu.edu>
2022-03-16 15:14:28 -0400

    addded abide data

---
+++ data/abide/abide_data.py
@@ -0,0 +1,26 @@
+import h5py
+import pandas as pd
+import numpy as np
+
+# read the data
+hf = h5py.File("ABIDE1_AllData.h5", "r")
+data = hf.get("ABIDE1_dataset")
+data = np.array(data)
+num_subjects = data.shape[0]
+num_components = 100
+data = data.reshape(num_subjects, num_components, -1)
+print("data shape: ", data.shape)
+
+# take only those brain networks that are not noise
+filename = "correct_indices_GSP.csv"
+df = pd.read_csv(filename, header=None)
+c_indices = df.values
+c_indices = c_indices.astype("int")
+c_indices = c_indices.flatten()
+c_indices = c_indices - 1
+finalData = data[:, c_indices, :]
+print("data shape: ", finalData.shape)
+
+filename = "labels_ABIDE1.csv"
+df = pd.read_csv(filename, header=None)
+labels = df.values.flatten() - 1
